af80f257f5fa046b1eea37dcd9f0dd084ef14428
ilya Pidonenko <ilja.pidonekom@gmail.com>
2023-11-08 13:36:37 +0100

    Product Routing added, buttons added

--- src/app/pages/product/product-routing.module.ts
+++ src/app/pages/product/product-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductModifyComponent } from './product-modify/product-modify.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product-list',
+    component: ProductListComponent
+  },
+  {
+    path: 'product-modify',
+    component: ProductModifyComponent
+  },
+  {
+    path: 'edit/:id',
+    component: ProductModifyComponent
+  },
+  {
+    path: ":id",
+    component: ProductDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductModifyComponent } from './product-modify/product-modify.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductModifyComponent,
+    ProductDetailComponent
+  ],
   imports: [
     CommonModule,
     ProductRoutingModule
