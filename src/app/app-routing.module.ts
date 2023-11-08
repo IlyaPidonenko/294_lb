0d617e99e5c0abd866e62d1cf25a7ac3d659b5fb
ilya Pidonenko <ilja.pidonekom@gmail.com>
2023-11-08 10:45:06 +0100

    Routing Anpassung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth/login",
+    loadComponent: () => import('./pages/auth/login/login.component').then(v => v.LoginComponent)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
